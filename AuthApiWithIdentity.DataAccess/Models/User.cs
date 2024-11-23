2376e6a4e1ba4cfabf37b7b69964e2e2feb7def5
pepi.iliev <pepi.iliev@gmail.com>
2024-11-24 01:53:40 +0200

    Add project files.

---
+++ AuthApiWithIdentity.DataAccess/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace UsersApi.DataAccess.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Identity;
+
+    public class User : IdentityUser
+    {
+        public override string Email { get; set; }
+        [StringLength(30, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [StringLength(30, MinimumLength = 2)]
+        public string LastName { get; set; }
+        public string? Image { get; set; }
+    }
+}
---
+++ AuthApiWithIdentity/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UsersApi.DataAccess.Models;
+using UsersApi.DataAccess;
+using UsersApi.DataAccess.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+    .AddCookie(IdentityConstants.ApplicationScheme)
+    .AddBearerToken(IdentityConstants.BearerScheme);
+
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddApiEndpoints();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.ApplyMigrations();
+}
+
+app.UseHttpsRedirection();
+
+app.MapIdentityApi<User>();
+
+app.MapControllers();
+
+app.Run();
